a22e45c885986d8a2aee2c45e4ad65658b426a6b
taobingxue <taobingxue001@126.com>
2017-03-08 15:06:52 -0500

    First version level/level control/list node

---
+++ Assets/Resources/Scripts/LevelController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Level {
+    public string init;
+    public string target;
+
+    /*  
+     * possibilities
+     *      minimum operations
+     *      hints
+     *      etc.
+     */
+
+    public static string get_char(string s) {
+
+    }
+
+}
+
+public class LevelController : MonoBehaviour {
+
+    public Level[] levels;
+    int idx = 0;
+     
+    public int max_size;
+    public GameObject[] lists;
+
+    public GameObject edge_prefab;
+    public GameObject node_prefab;
+
+    public GameObject[] templates;
+
+    private void Awake() {
+        lists = new GameObject[max_size];
+        for (int i = 0; i < max_size; ++i) {
+            lists[i] = null;
+        }
+
+        create_level();
+    }
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void level_up() {
+
+    }
+
+    void create_level() {
+
+    }
+
+    void clean_level() {
+
+    }
+}
---
+++ Assets/Resources/Scripts/ListNode.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListNode : MonoBehaviour {
+
+    public bool is_template;
+    public GameObject[] selected_ring;
+    GameObject[] ring;
+
+    public char ch = ' ';
+    public GameObject next_node = null;
+
+    private void Awake() {
+        ring = new GameObject[2];
+        ring[0] = null;
+        ring[1] = null;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void set_character(char _ch) {
+        transform.FindChild("Canvas").FindChild("flagletter").gameObject.GetComponent<Text>().text = _ch + "";
+        ch = _ch;
+    }
+
+    public void select(int ty = 0) {
+        if (ring[ty] != null) {
+            Debug.Log("Hehe!");
+        }
+        ring[ty] = Instantiate(selected_ring[ty]) as GameObject;
+        ring[ty].transform.parent = transform;
+        ring[ty].transform.localPosition = Vector3.zero;
+        ring[ty].transform.localScale = Vector3.one;
+    }
+
+    public void unselect(int ty = 0) {
+        if (ring[ty] == null) {
+            Debug.Log("Hehe!");
+            return;
+        }
+
+        Destroy(ring[ty]);
+        ring[ty] = null;
+    }
+}
